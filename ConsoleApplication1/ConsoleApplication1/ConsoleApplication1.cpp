c3f7adc5212d34f1331514e17d74c6a642201b04
Valeria Polozun <valeria.polozun@gmail.com>
2013-05-05 21:31:27 +0200

    rename test files

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,18 +10,20 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	
-	//TourDefault td("kicsitesztdl3.txt"); // Expected tour: {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//TourDefault td("smalltest_dl3.txt"); // Expected tour: {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	//TourNearestNeighbor tnd("kicsitesztdl3.txt"); // Expected tour : { 0,2,3,4, 6, 5, 7, 8, 9, 10}
+	//TourNearestNeighbor tnd("smalltest_dl3.txt"); // Expected tour : { 0,2,3,4, 6, 5, 7, 8, 9, 10}
 
 	//TourFirstPickupSecondDelivery tfirstpickupseconddelivery("testFirstPickupSecondDeliverydl1000.txt"); // Expected tour: { 0,2,4,5,6,1}
 
-	//TourNearestNeighborDistanceLimit tnndl3("kicsitesztdl3.txt"); // Expected tours : no tours
-	//TourNearestNeighborDistanceLimit tnndl5("kicsitesztdl5.txt"); // Expected tours : {0,2,1} 
-	//TourNearestNeighborDistanceLimit tnndl8("kicsitesztdl8.txt"); // Expected tours : {0,2,3,4,1} , {0,5,1}
+	//TourNearestNeighborDistanceLimit tnndl3("smalltest_dl3.txt"); // Expected tours : no tours
+	//TourNearestNeighborDistanceLimit tnndl5("smalltest_dl5.txt"); // Expected tours : {0,2,1} 
 	
-	TourNearestNeighborDistanceLimit tnndlbig("set_64_1_50_300.txt"); // 
-	tnndlbig.runShakingAndTwoopt();
+	TourNearestNeighborDistanceLimit tnndl8("smalltest_dl8.txt"); // Expected tours : {0,2,3,4,1} , {0,5,1}
+	tnndl8.runShakingAndTwoopt();
+
+	//TourNearestNeighborDistanceLimit tnndlbig("set_64_1_50_300.txt"); // 
+	//tnndlbig.runShakingAndTwoopt();
 
 	int k;
 	cin >> k;
